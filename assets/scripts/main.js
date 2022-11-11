0d4bcb4b94b216477158283b12fcce4fec2ef841
Pethaperumal Natarajan <pnataraj@ucsd.edu>
2022-11-10 17:57:41 -0800

    Attempted to fix github pages

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -52,9 +52,7 @@ function initializeServiceWorker() {
         try {
           // B3. TODO - Register './sw.js' as a service worker (The MDN article
           //            "Using Service Workers" will help you here)
-          const registration = await navigator.serviceWorker.register("./sw.js", {
-            scope: "/",
-          });
+          const registration = await navigator.serviceWorker.register("./sw.js");
           // B4. TODO - Once the service worker has been successfully registered, console
           //            log that it was successful.
           console.log("Registration sucessful");
